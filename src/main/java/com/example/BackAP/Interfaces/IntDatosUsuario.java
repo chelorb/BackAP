1d330c20285044ae1db1110a7b63f253d7812667
chelorb <chelorb@hotmail.com>
2022-07-24 23:33:50 -0300

    Servicio (Datos Usuario)

--- src/main/java/com/example/BackAP/Interfaces/IntDatosUsuario.java
+++ src/main/java/com/example/BackAP/Interfaces/IntDatosUsuario.java
@@ -1,4 +1,15 @@
 package com.example.BackAP.Interfaces;
 
+import com.example.BackAP.Entidades.DatosUsuario;
+
+import java.util.List;
+
 public interface IntDatosUsuario {
+    List<DatosUsuario> getUsuario();
+
+    void saveDatosUsuario(DatosUsuario usuario);
+
+    void deleteUsuario(Integer id);
+
+    DatosUsuario findUsuario(Integer id);
 }
--- src/main/java/com/example/BackAP/Servicios/DatosUsuarioServ.java
+++ src/main/java/com/example/BackAP/Servicios/DatosUsuarioServ.java
@@ -1,4 +1,45 @@
 package com.example.BackAP.Servicios;
 
-public class DatosUsuarioServ {
+import com.example.BackAP.Entidades.DatosUsuario;
+import com.example.BackAP.Interfaces.IntDatosUsuario;
+import com.example.BackAP.Repositorio.DatosUsuarioRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DatosUsuarioServ implements IntDatosUsuario {
+
+    @Autowired
+    DatosUsuarioRepo datosUsuarioRepo;
+
+
+    @Override
+    public List<DatosUsuario> getUsuario(){
+
+        List<DatosUsuario> listaUsuario=datosUsuarioRepo.findAll();
+        return listaUsuario;
+    }
+    @Override
+    public void saveDatosUsuario(DatosUsuario usuario){
+        datosUsuarioRepo.save(usuario);
+
+    }
+
+    @Override
+    public void deleteUsuario(Integer id){
+
+        datosUsuarioRepo.deleteById(id);
+    }
+
+    @Override
+    public DatosUsuario findUsuario(Integer id) {
+DatosUsuario usuario=datosUsuarioRepo.findById(id).orElse(null);
+return usuario;
+    }
+
+
+
+
 }
