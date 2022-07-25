3488247571de8eee81825b522fdf6228ac01c666
chelorb <chelorb@hotmail.com>
2022-07-24 23:21:46 -0300

    Servicio (Proyecto)

--- src/main/java/com/example/BackAP/Interfaces/IntProyecto.java
+++ src/main/java/com/example/BackAP/Interfaces/IntProyecto.java
@@ -1,4 +1,17 @@
 package com.example.BackAP.Interfaces;
 
+import com.example.BackAP.Entidades.Proyecto;
+
+import java.util.List;
+
 public interface IntProyecto {
+    List<Proyecto> getProyecto();
+
+    void saveProyecto(Proyecto proyecto);
+
+    void deleteProyecto(Proyecto proyecto);
+
+    void deleteProyecto(Integer id);
+
+    Proyecto findProyecto(Integer id);
 }
--- src/main/java/com/example/BackAP/Servicios/ProyectoServ.java
+++ src/main/java/com/example/BackAP/Servicios/ProyectoServ.java
@@ -1,4 +1,49 @@
 package com.example.BackAP.Servicios;
 
-public class ProyectoServ {
+import com.example.BackAP.Entidades.Proyecto;
+import com.example.BackAP.Interfaces.IntProyecto;
+import com.example.BackAP.Repositorio.ProyectoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProyectoServ implements IntProyecto {
+
+
+    @Autowired
+    private ProyectoRepo proyectoRepo;
+
+
+    @Override
+    public List<Proyecto> getProyecto() {
+
+        List<Proyecto> listaProyecto = proyectoRepo.findAll();
+        return listaProyecto;
+    }
+
+    @Override
+    public void saveProyecto(Proyecto proyecto) {
+
+        proyectoRepo.save(proyecto);
+    }
+
+    @Override
+    public void deleteProyecto(Proyecto proyecto) {
+
+    }
+
+    @Override
+    public void deleteProyecto(Integer id) {
+        proyectoRepo.deleteById(id);
+    }
+
+    @Override
+    public Proyecto findProyecto(Integer id) {
+        Proyecto proyecto = proyectoRepo.findById(id).orElse(null);
+        return proyecto;
+
+
+    }
 }
