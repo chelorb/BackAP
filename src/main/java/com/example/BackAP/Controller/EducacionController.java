7f3cbf4955fe2a455349cbee0c00ef701adb74c4
chelorb <chelorb@hotmail.com>
2022-07-25 00:35:47 -0300

    Educacion Controller

--- src/main/java/com/example/BackAP/Controller/EducacionController.java
+++ src/main/java/com/example/BackAP/Controller/EducacionController.java
@@ -1,4 +1,50 @@
 package com.example.BackAP.Controller;
 
+import com.example.BackAP.Entidades.Competencia;
+import com.example.BackAP.Entidades.Educacion;
+import com.example.BackAP.Interfaces.IntCompetencia;
+import com.example.BackAP.Interfaces.IntEducacion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class EducacionController {
+
+    @Autowired
+    IntEducacion intEducacion;
+    @GetMapping("/educacion/traer")
+
+    public List<Educacion> getEducacion(){
+
+        return intEducacion.getEducacion();
+    }
+
+
+    @GetMapping("/educacion/trer/{id}")
+
+    public Educacion findEducacion (@PathVariable Integer id) {
+        Educacion educacion=intEducacion.findEducacion(id);
+        return educacion;
+    }
+
+    @PostMapping("/educacion/crear")
+
+    public void crearEducacion(@RequestBody Educacion educacion){
+
+       intEducacion.saveEducacion(educacion);
+    }
+
+    @DeleteMapping("/educacion/borrar/{id}")
+    public void borrarEducacion(@PathVariable Integer id){
+
+        intEducacion.deleteEducacion(id);
+    }
+
+    @PutMapping("/educacion/editar/{id}")
+    public void actualizarEducacion(@RequestBody Educacion educacion) {
+       intEducacion.saveEducacion(educacion);
+    }
+
 }
